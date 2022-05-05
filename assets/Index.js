629459935a95611b4e74debcb53213a52a1c0c05
RAZAFISALAMA <98509559+RAZAFISALAMA@users.noreply.github.com>
2022-05-05 13:08:35 +0300

    Add files via upload

---
+++ assets/Index.js
@@ -0,0 +1,67 @@
+let menu=document.querySelector("li");
+console.log(menu);
+menu.forEach(fuction)
+{
+    li.addEventListernon("mouseover") ,()=> li
+};    
+li.style.textedécoration="underline"; 
+li.style="none";
+$(document).realy(function(){
+
+});
+$(document).ready(fonction());
+    
+
+var btn1=document.getElementById("button");
+var btn2=document.getElementById("button");                  
+function btn_2hover(){
+    btn2.style.backgroundColor ="#fff";
+    btn2.style.color="#b11313";
+}
+function btn_2hoverleave(){
+    btn1.style.backgroundColor="";
+    btn1.style=color="#fff";
+
+}
+function btn1_leave(){
+    btn1.style.backgroundColor="#fff";
+btn1.style.color="#b11313";
+
+}
+var icon= document.getElementByFooter-icon("icon");
+function icon_hover(){
+    icon.style.backgroundColor="red";
+    icon.text.color=" var----white";
+}
+$(document).ready(function(){
+    $("marveltext").fadein();
+    $("marveltext").animated({
+        left:"400px"
+    }("slow")
+})
+$(document).ready;
+$("window").scroll(fuction())
+{
+    let scroll=$("window");
+}
+console.log(scroll);
+$("#Marvelimg").css("scroll,px");
+ var modal=document.getElementById(myModal);
+ var send =document.getElementById("sendModal");
+ sendModal.onclik=fuction();
+     modal.style;
+     vari=0;
+     var images=[ "heroSlider1","heroSlider2","heroSlider3","heroslider4"];
+     console.log(images)
+ ;
+ let footer_icon=document .getElementBy("footer_icon");
+ btninverse[i].addEventListernon("mouseover"),++i
+     btninverse[i].style.backgroundColor ="red";
+     btninverse[i].style-color=="white";
+     btninverse[i].style-fontweith=="bolde";
+     btninverse[i].box-chadow=="5px,3px,30px,white";
+     btninverse[i].addEventListernon("uncursear"()= i);
+     btninverse[i].style.backgroundColor="white";
+     btninverse[i].style -color=="red";
+     btninverse[i].box-shadow=="brow";
+ }
